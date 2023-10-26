1608edcecafeae10310e5990521e5bcab456fa8f
wayne tsao <a578ff6@gmail.com>
2023-10-26 21:07:10 +0800

    Initial Commit

---
+++ MyLabContest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLabContest
+//
+//  Created by 曹家瑋 on 2023/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
